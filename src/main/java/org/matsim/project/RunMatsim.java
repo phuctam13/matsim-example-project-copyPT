4497c266135607361ba0b14aab98a4d06c366fac
phuctam13 <56546882+phuctam13@users.noreply.github.com>
2020-05-04 18:38:22 +0200

    remove agents and add a new one with a simple plan

--- src/main/java/org/matsim/project/RunMatsim.java
+++ src/main/java/org/matsim/project/RunMatsim.java
@@ -18,11 +18,17 @@
  * *********************************************************************** */
 package org.matsim.project;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author nagel
@@ -37,11 +43,46 @@ public class RunMatsim{
 		// possibly modify config here
 		
 		// ---
-		
+
 		Scenario scenario = ScenarioUtils.loadScenario(config) ;
 		
 		// possibly modify scenario here
-		
+		Id<Person> interestingPersonID = Id.createPersonId(1);
+		List<Id<Person>> personsToRemove = new ArrayList<>();
+
+		for(Id<Person> personID : scenario.getPopulation().getPersons().keySet()){
+			if(!personID.equals(interestingPersonID)){
+				personsToRemove.add(personID);
+			}
+		}
+
+		for (Id<Person> personId:personsToRemove){
+			scenario.getPopulation().removePerson(personId);
+		}
+
+		System.out.println("Population size = " + scenario.getPopulation().getPersons().size());
+
+		PopulationFactory populationFactory = scenario.getPopulation().getFactory();
+
+		Person person2 = populationFactory.createPerson(Id.createPersonId("PhucTam"));
+
+		Plan plan = populationFactory.createPlan();
+
+		Activity homeActivity = populationFactory.createActivityFromLinkId("h", Id.createLinkId(21));
+		homeActivity.setEndTime(6*60*60.);
+		plan.addActivity(homeActivity);
+
+		Leg leg = populationFactory.createLeg(TransportMode.car);
+		plan.addLeg(leg);
+
+		Activity workActivity = populationFactory.createActivityFromLinkId("w", Id.createLinkId(1));
+		plan.addActivity(workActivity);
+
+		person2.addPlan(plan);
+		scenario.getPopulation().addPerson(person2);
+
+		System.out.println("Population size = " + scenario.getPopulation().getPersons().size());
+
 		// ---
 		
 		Controler controler = new Controler( scenario ) ;
